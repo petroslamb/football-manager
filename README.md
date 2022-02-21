5aa7f3e1800c76766c2d0556b2a23f58df54b13c
Petros Lamb <petroslamb@gmail.com>
2022-02-22 01:17:57 +0200

    Organize files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# football-manager
-Kaggle Match Outcome Prediction in Football
+# Kaggle Match Outcome Prediction in Football
+
+The goal of this project is to predict match outcomes of European football matches more accurately than bookkeepers, thereby beating the odds and to, in the end, generate a positive return on investment. Based on the dataset provided on kaggle.com that includes basic match data, FIFA player statistics and bookkeeper data, I built a model to predict the probability of each match outcome – win, draw, or defeat.
+
+https://www.kaggle.com/hugomathien/soccer
+
+## Usage
+
+- Create a new environment and clone the repo.
+- Install the dependencies with `pip install -r requirements.txt`.
+- Run the notebooks in order to train the model and predict the outcome of the matches.
--- explore.sql
+++ explore.sql
@@ -1,24 +1,34 @@
--- SQLite
+-- SQLite database exploration
+-- Select the rows from the match table, according to the criteria
 SELECT 
-id, date, match_api_id, 
-home_team_api_id, away_team_api_id, 
+id, 
+date, 
+match_api_id, 
+-- keep team ids to join with team attributes table later
+home_team_api_id, 
+away_team_api_id, 
+-- keep the difference in goals as a feature
 (home_team_goal - away_team_goal) as goal_diff,
+-- create a column with the result of the match
  CASE 
     WHEN home_team_goal > away_team_goal THEN 'H'
     WHEN home_team_goal < away_team_goal THEN 'A' 
     ELSE 'D'
     END as win_lose_draw,
- B365H, B365D, B365A, BWH, BWD, BWA, IWH, IWD, IWA, LBH, LBD, LBA
- -- ,  WHH, WHD, WHA, SJH, SJD, SJA, VCH, VCD, VCA, GBH, GBD, GBA, BSH, BSD, BSA
- -- join with team_attributes and keep only the attributes we need
+-- keep the odds given by the bookies as a feature (4 bookies) 
+B365H, B365D, B365A, BWH, BWD, BWA, IWH, IWD, IWA, LBH, LBD, LBA
+-- ,  WHH, WHD, WHA, SJH, SJD, SJA, VCH, VCD, VCA, GBH, GBD, GBA, BSH, BSD, BSA
 FROM Match;
 
+-- Select the team attributes, keep only the attributes we need
 SELECT
 id, team_api_id, date, buildUpPlaySpeed, buildUpPlayPassing, chanceCreationPassing, chanceCreationCrossing, chanceCreationShooting, defencePressure, defenceAggression, defenceTeamWidth
 FROM Team_Attributes;
 
+-- Count the number of different matches played and the date ranges of the matches, aka the number of seasons
 SELECT
 count(distinct(date)), min(date), max(date) FROM Match;
 
+-- Check the number of dates we have for team attributes, aka the number of seasons.
 SELECT
 DISTINCT(date) FROM Team_Attributes;
